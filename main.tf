df61260b88c0ed62d53b7a9b012df38a30e4551c
henrique <henrique.farinha@gmail.com>
2025-02-06 12:02:00 -0500

    add LB

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
@@ -44,6 +44,33 @@ resource "aws_instance" "web" {
 
   tags = {
     Name = "HelloWorld"
+  }
+}
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+
+  vpc_id  	  = module.blog_vpc.vpc_id
+  subnets 	  = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  target_groups = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
+    }
+
+  listeners = {
+      port     = 80
+      protocol = "HTTP"
+      }
+
+  tags = {
+    Environment = "dev"
   }
 }
 
@@ -60,30 +87,3 @@ module "blog_sg" {
   egress_rules	      = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
-
-#resource "aws_security_group" "blog" {
-#  name        = "blog"
-#  description = "Allow http and https in. Allow everything out"
-#
-#  vpc_id = data.aws_vpc.default.id
-#}
-#
-#resource "aws_security_group_rule" "blog_http_in" {
-#  type        = "ingress"
-#  from_port   = 80
-#  to_port     = 80
-#  protocol    = "tcp"
-#  cidr_blocks = ["0.0.0.0/0"]
-#
-#  security_group_id = aws_security_group.blog.id
-#}
-#
-#resource "aws_security_group_rule" "blog_everything_out" {
-#  type        = "egress"
-#  from_port   = 0
-#  to_port     = 0
-#  protocol    = "-1"
-#  cidr_blocks = ["0.0.0.0/0"]
-#
-#  security_group_id = aws_security_group.blog.id
-#}
